d1427c50980d52baf02bbe3a441af857ad774f6f
anjireddygopu <37733662+anjireddygopu@users.noreply.github.com>
2019-07-01 12:05:38 +0530

    Create MethodReference1.java

---
+++ MethodReference1.java
@@ -0,0 +1,20 @@
+
+//MethodReference to instance method (ClassName::MethodName)
+
+import java.util.function.Function;
+ 
+public class Java8MethodReferences
+{
+    public static void main(String[] args) 
+    {
+        //Calling toLowerCase() method using lambda
+         
+        Function<String, String> lambdaFunction = (String s) -> s.toLowerCase();
+        System.out.println(lambdaFunction.apply("JAVA"));
+         
+        //Calling toLowerCase() method using method reference
+         
+        Function<String, String> referenceFunction = String::toLowerCase;
+        System.out.println(referenceFunction.apply("JAVA"));
+    }
+}
